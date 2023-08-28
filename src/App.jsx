3911dfc67a35a6ab292cd410f1c3c7acde818c7f
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-28 18:23:46 +0100

    default context state

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import "./App.css";
 import useCurrentUser from "./customHooks/useCurrentUser";
 import CustomersList from "./components/Records";
 
-const AppContent = createContext();
+const AppContent = createContext({
+  user: null,
+  isFetchingUser: false
+});
 
 export const useAppContext = () => useContext(AppContent);
 function App() {
